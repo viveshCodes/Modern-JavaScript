64afe92ab5a8967704625b44b5d839fecf5198f5
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-13 20:09:51 +0545

    Add numbers

---
+++ Tutorial/03-strings.js
@@ -0,0 +1,17 @@
+// strings
+let email1 = "viveshofficial@gmail.com"
+let email2 = "vivesh.181CO259@nitk.edu.in"
+
+// string concatenation
+let twoEmails = email1 + " "+ email2
+console.log(twoEmails)
+
+// getting characters
+console.log(email1[7])
+
+// string length
+let noOfCharac = email1.length;  // here , length is a property
+console.log(noOfCharac);
+
+// string methods - methods are functions associated with particular object
+console.log(email1.toUpperCase());
---
+++ Tutorial/05-numbers.js
@@ -0,0 +1,38 @@
+let radius = 7
+const pie = 3.14
+let perimeter = 2 * pie * radius
+console.log(perimeter)
+
+/*________________________________
+ Math Operators Available To Us:
+___________________________________
++
+-
+*
+/
+**  power
+%
+
+*/
+
+
+/*___________________
+Order Of Operations :
+______________________
+B I D M A S
+*/
+
+
+/*________________________________
+Increment and Decrement Operators:
+__________________________________
+
+++
+--
+
+*/
+
+
+let likes = 10;
+likes ++ ;
+console.log(likes);
