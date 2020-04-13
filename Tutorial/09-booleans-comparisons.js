51d9eb8c6f5215b312dcd1b9d40d3b80633397f6
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-13 20:59:03 +0545

    Add booleans and comparisons

---
+++ Tutorial/09-booleans-comparisons.js
@@ -0,0 +1,23 @@
+// Booleans : true / false
+
+let email = "viveshofficial@gmail.com"
+let names = ['Vivesh' ,'viveshCodes' , 'viv']
+
+// Method For Boolean
+let result1 = email.includes('@') // to check if the particular character is present
+console.log(result1)
+let result2 = names.includes('viv-bhai')
+console.log(result2)
+
+
+/*__________________
+Comparison Operators:
+_____________________
+
+==  Equal to
+!=  Not equal to
+>   Greater than
+>=  Greater than or equal to
+<   Less than 
+<=  Less than or equal to
+*/
