24fb54209add90afd5ce6fa84572e3b1e26c5bde
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-14 12:50:28 +0545

    Add if concept

---
+++ Tutorial/Control-Flow/04-if.js
@@ -0,0 +1,8 @@
+let age = 25
+if(age <= 20){
+    console.log("You need to study a lot")
+}else if (age < 50 && age >20 ){
+    console.log("You need to work a lot")
+}else{
+    console.log("You need to relax" )
+}
