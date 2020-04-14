c52a5fdaa0e21577f5a3e9cac4e043da8b61fe51
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-14 12:38:42 +0545

    Add for loop

---
+++ Tutorial/Control-Flow/01-for-loop.js
@@ -0,0 +1,10 @@
+for (let i = 0 ; i< 5 ; i++){
+    console.log("Hello JS , #", i+1)
+}
+
+const players = ['Mahi Bhai' , 'Watto' , 'Faf']
+for(let i = 0 ; i <players.length ; i ++){
+    console.log(player[i])
+    let html = <div>${player[i]}</div>
+    console.log(html)
+}
