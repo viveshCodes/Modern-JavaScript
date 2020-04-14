af3365bc43fa3e1c9be82b20ad243528bc1ed759
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-14 12:28:18 +0545

    Add type conversion

---
+++ Tutorial/JavaScript-Basics/11-type-conversion.js
@@ -0,0 +1,16 @@
+let age = 38
+let luckyYear = '2011'
+
+// Number To String
+let result1 = String(age)
+console.log(result1 , typeof result1)
+
+// String To Number
+let result2 = Number(luckyYear)
+console.log(result2 , typeof result2)
+
+/*
+Output :
+38 string
+2011 number
+*/
