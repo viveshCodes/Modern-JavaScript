3b7a5c20dc22063e32566795f79c0e7a0330db65
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-14 12:58:35 +0545

    Add logical operators

---
+++ Tutorial/Control-Flow/05-logical-operators.js
@@ -0,0 +1,10 @@
+// Logical operators - || OR  , && AND
+let number = 7
+let player = 'Mahi Bhai'
+if(number === 7 && player === "Mahi Bhai"){
+    console.log("You're true MSDian")
+}else if(number === 7 || player === "Mahi Bhai"){
+    console.log("You like Mahi Bhai")
+}else {
+    console.log("You don't have knowledge of cricket")
+}
