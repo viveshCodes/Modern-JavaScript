13ba96dbe7a8dc0809114fd3da81b18b88a794fc
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-14 13:11:21 +0545

    Add switch statement

---
+++ Tutorial/Control-Flow/08-switch.js
@@ -0,0 +1,14 @@
+let grade = 'B'
+switch(grade){
+    case 'A':
+        console.log("You got grade A")
+        break
+    case 'B':
+        console.log("You got grade B")
+        break   
+    case 'C':
+        console.log("You got grade C")
+        break      
+    default:
+        console.log("Not a valid grade")     
+}
