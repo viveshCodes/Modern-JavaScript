3c397163b4cebdad10d1faee9d1f673fcc6dc9b2
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-15 19:57:45 +0545

    Add math object

---
+++ Tutorial/Objects-Literals/06-math-object.js
@@ -0,0 +1,16 @@
+let radius = 7
+let area = Math.PI * radius**2
+console.log(area)
+
+// Math Object Available In JavaScript
+console.log(Math.PI)
+console.log(Math.E)
+console.log(Math.round(area))
+console.log(Math.floor(4.6))
+console.log(Math.ceil(5.3))
+console.log(Math.trunc(5.3))   // Give only integer part
+
+const randomNumber = Math.random(); // Generate random number between 0 & 1
+console.log(randomNumber)
+console.log(Math.round(randomNumber))  // It'll generate either 0 or 1
+console.log(Math.round(randomNumber * 100))
