91cbacab0768c5a20ce5da6d93470f0730a34229
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-15 18:45:39 +0545

    Add object

---
+++ Tutorial/Objects-Literals/01-create-object.js
@@ -0,0 +1,20 @@
+let user = {
+    name :"viveshCodes",
+    email :"viveshofficial@gmail.com",
+    location : "Bangalore",
+    blogs :["JavaScript From Novice To Ninja"]
+}
+console.log(user)
+
+// grab name of user in two ways
+console.log(user.name)
+console.log(user['name'])
+
+// find the data type of object
+console.log(typeof user)
+
+/*
+NOTE :
+Properties come with key and value pair 
+key : value
+*/
