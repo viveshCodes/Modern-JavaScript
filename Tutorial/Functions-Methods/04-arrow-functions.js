e6b702c187708380f93e5a50d772c952b6a9c2bb
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-15 15:21:15 +0545

    Add arrow function

---
+++ Tutorial/Functions-Methods/04-arrow-functions.js
@@ -0,0 +1,19 @@
+/*
+//Function expression ,
+const areaOfCircle = function(radius){
+    return pie * radius**2
+
+};
+const pie = 3.14
+const area = areaOfCircle(7)
+console.log("The area of circle is ", area)
+
+*/
+
+//Arrow Function
+const areaOfCircle = (radius) =>{
+    return pie * radius**2;
+};
+const pie = 3.14
+const area = areaOfCircle(7)
+console.log("The area of circle is ", area)
