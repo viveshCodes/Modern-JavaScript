460af4e1bffbbd0958e48fb67fe9642a5f477f5b
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-15 16:15:26 +0545

    Add callback in action

--- Tutorial/sandbox.js
+++ Tutorial/sandbox.js
@@ -1,2 +1,13 @@
-let mahi = 7 ;
-document.write("mahi");
+const ul = document.querySelector('.people');
+
+const people = ['Mahi Bhai', 'MS Dhoni', 'Thala', 'Captain Cool', 'Finisher'];
+
+let html = ``;
+
+people.forEach(person => {
+  // create html template for each person
+  html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
