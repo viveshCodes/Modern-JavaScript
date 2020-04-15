bc38c86c9c476e892dfd83414e5394c509de0262
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-15 18:50:36 +0545

    Add methods

---
+++ Tutorial/Objects-Literals/02-adding-methods.js
@@ -0,0 +1,14 @@
+let user = {
+    name :"viveshCodes",
+    email :"viveshofficial@gmail.com",
+    location : "Bangalore",
+    blogs :["JavaScript From Novice To Ninja"],
+    login : function(){
+        console.log("You're logged in .")
+    },
+    logout: function(){
+        console.log("You'll be logged out .")
+    }
+}
+user.login();
+user.logout();
