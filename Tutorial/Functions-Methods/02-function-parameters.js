34154455d46720a900269a232c504251da0ab992
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-15 15:15:02 +0545

    Add parameters and returning values

---
+++ Tutorial/Functions-Methods/02-function-parameters.js
@@ -0,0 +1,4 @@
+const speak = function(name){     // On this lne , name is parameter
+    console.log(`Good morning ${name}`)
+}
+speak("Mahi Bhai") // Here , Mahi Bhai is argument
---
+++ Tutorial/Functions-Methods/03-returning-values.js
@@ -0,0 +1,7 @@
+const perimeterOfCircle = function(radius){
+    return 2 * pie * radius
+
+}
+const pie = 3.14
+let perimeter = perimeterOfCircle(7)
+console.log("The perimeter of circle is ", perimeter)
