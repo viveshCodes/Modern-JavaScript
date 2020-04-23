3bab9a83e4104e845ea615c1b5e856dfbab946a5
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-23 11:41:22 +0545

    Add digital clock

--- Tutorial/Digital Clock/app.js
+++ Tutorial/Digital Clock/app.js
@@ -1,1 +1,14 @@
 const clock = document.querySelector('.clock');
+const tick = () =>{
+    const now = new Date();
+    const hour = now.getHours(); 
+    const minute = now.getMinutes(); 
+    const second = now.getSeconds(); 
+    const html = `
+    <span>${hour}</span> : 
+    <span>${minute}</span> :
+    <span>${second}</span> 
+    `;
+    clock.innerHTML = html ;
+};
+setInterval(tick , 1000);
