49b4db9ce534a236024b9303005689f9e362289a
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-28 11:47:43 +0545

    Add super

---
+++ Tutorial/OOP/Super/sandbox.js
@@ -0,0 +1,54 @@
+/*_____________________________
+CLASS is a blueprint for OBJECT.
+It describes how one should be made.
+_______________________________*/
+
+class User {
+    constructor(username , email){
+        // setup properties
+        this.username = username;
+        this.email = email;
+        this.score = 0;
+    } // Unlike Object Literal , we don't use comma here
+    login(){
+        console.log(`${this.username} is logged in`);
+        return this ; // this = object instance
+    }  
+    logout(){
+        console.log(`${this.username} is logged out`)
+        return this;
+    }
+    inScore(){
+        this.score += 1;
+        console.log(`${this.username} has score of ${this.score}`);
+        return this;
+    }
+
+}
+
+//Let's create a subclass
+// subclass extends another class
+class Admin extends User{
+    constructor(username , email , title){
+        super(username , email) ; // from parent class
+        this.title = title;
+    }
+    deleteUser(user){
+       users =users.filter(u => u.username !== user.username);
+       } 
+}
+
+
+const userOne = new User('vivesh' , 'viveshofficial@gmail.com');
+const userTwo = new User('viv' , 'viv@gmail.com');
+
+const userThree = new Admin('viveshCodes' , 'viveshCodes@gmail.com','developer');
+console.log(userThree);
+console.log(userThree.title);
+
+let users = [userOne , userTwo , userThree];
+
+console.log(userOne ,userTwo , userThree);
+
+userThree.deleteUser(userTwo);
+console.log(users);
